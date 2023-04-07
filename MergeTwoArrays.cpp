e2a22862accf138e0e52a144eb67b96ba7ea7ab0
am0654 <94629609+am0654@users.noreply.github.com>
2023-04-08 00:00:32 +0530

    Create MergeTwoArrays.cpp

---
+++ MergeTwoArrays.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arrOne[50], arrTwo[50], arrMerge[100];
+	int sizeOne, sizeTwo, i, k;
+    cout<<"Enter the Size for First Array: ";
+    cin>>sizeOne;
+    cout<<"Enter "<<sizeOne<<" Elements for First Array: ";
+    for(i=0; i<sizeOne; i++)
+    {
+        cin>>arrOne[i];
+        arrMerge[i] = arrOne[i];
+    }
+    k = i;
+    cout<<"\nEnter the Size for Second Array: ";
+    cin>>sizeTwo;
+    cout<<"Enter "<<sizeTwo<<" Elements for Second Array: ";
+    for(i=0; i<sizeTwo; i++)
+    {
+        cin>>arrTwo[i];
+        arrMerge[k] = arrTwo[i];
+        k++;
+    }
+    cout<<"\nThe New Array (Merged Array):\n";
+    for(i=0; i<k; i++)
+        cout<<arrMerge[i]<<" ";
+    cout<<endl;
+    return 0;
+}
+
+
+
+Input:
+Enter the Size for First Array: 5
+Elements for First Array: 1 5 4 6 8
+
+Enter the Size for Second Array: 5
+Elements for Second Array: 4 5 6 8 7
+
+Output:
+The New Array (Merged Array):1 5 4 6 8 4 5 6 8 7
